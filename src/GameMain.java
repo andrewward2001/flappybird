c4d7708e991b4ec01a6ca6588864c404dcb467ed
andrewward2001 <tmiandreww@gmail.com>
2017-03-15 09:54:50 -0400

    Add all the things

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,10 +1,85 @@
-/**
- * Created by andrew_ward on 3/15/17.
- */
-public class GameMain {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
-        System.out.println();
+public class GameMain extends JPanel {
+
+    //instance fields for the general environment
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
+    private Timer timer;
+    private boolean[] keys;
+
+    public GameMain(){
+
+        keys = new boolean[512]; //should be enough to hold any key code.
+        //initialize the instance fields.
+
+
+
+        timer = new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                //move the frog
+                if(keys[KeyEvent.VK_W]){
+                }
+
+                //update each obstacle
+
+                //check for collisions
+
+
+                repaint();
+            }
+        });
+        timer.start();
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = true;
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = false;
+            }
+        });
+
     }
 
+    //Our paint method.
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        //draw all the things.
+//        for(Sprite s: obstacles){
+//            s.draw(g2);
+//        }
+
+    }
+
+    //sets ups the panel and frame.
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Frogger!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        GameMain panel = new GameMain();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
 }

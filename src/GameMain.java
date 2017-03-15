ec884dd8b615dd194a7d1174363b34c344dff244
andrewward2001 <tmiandreww@gmail.com>
2017-03-15 10:31:00 -0400

    Rename window (why not)

--- src/GameMain.java
+++ src/GameMain.java
@@ -68,7 +68,7 @@ public class GameMain extends JPanel {
 
     //sets ups the panel and frame.
     public static void main(String[] args) {
-        JFrame window = new JFrame("Frogger!");
+        JFrame window = new JFrame("The Game Where Ben Does All the Work");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
 

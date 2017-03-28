488a64ad7eaaac9ba13621010b92caa30c605bbf
Benjamin Shapiro <mozartben@gmail.com>
2017-03-28 08:35:28 -0400

    added pipeset

--- src/PipeSet.java
+++ src/PipeSet.java
@@ -1,27 +1,24 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class PipeSet {
 
     private static final int fh = GameMain.FRAMEHEIGHT;
+    private int topPipeY, botPipeY;
+    private BufferedImage topPipe, botPipe;
 
     public PipeSet(){
-
+        int i = 3;
+        setupHeights();
     }
 
-    private String setupPic(boolean isTop){
-        if(isTop)
-            return "topPipe.png";
-        return "botPipe.png";
-    }
-
-    private int setupHeights(boolean isTop){
-        int topPipeY = (int)(Math.random()*700);
-        int botPipeY = fh - topPipeY + 100;
-
+    private int setupHeights(){
+        topPipeY = (int)(Math.random()*700);
+        botPipeY = fh - topPipeY + (int)(Math.random()*10) + 90;
     }
 
     public void draw(Graphics2D g2, PipeSet pipeSet){
-
+        g2.fillRoundRect();
     }
 
 }
